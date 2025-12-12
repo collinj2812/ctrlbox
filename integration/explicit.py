5a5a8e5497c4478852b55367132e1d9e99b0f469
collinj2812 <collin.johnson@tu-dortmund.de>
2025-12-12 13:53:02 +0100

    changed rk to single step

--- integration/explicit.py
+++ integration/explicit.py
@@ -1,35 +1,31 @@
 import numpy as np
 import time
 
-def rk4(sys, x0, u = None, t_span=(0, 1), dt = 0.1):
+def rk4(sys, x, u = None, t=None, dt = 0.1):
     # check inputs for consistency
-    assert len(x0) == sys.n_states
+    assert len(x) == sys.n_states
 
     time_start = time.perf_counter()
-    n_steps = int((t_span[1]-t_span[0]) / dt)
-    t = np.linspace(*t_span, n_steps + 1)
-    sol = np.zeros((n_steps + 1, sys.n_states))
-    sol[0] = x0
-    for step_i in range(1, n_steps + 1):
-        t_i = step_i*dt + t_span[0]
-        if u:
-            k1 = sys.rhs(t_i, sol[step_i - 1], u[step_i - 1])
-            k2 = sys.rhs(t_i + dt / 2, sol[step_i - 1] + dt * k1 / 2, u[step_i - 1])
-            k3 = sys.rhs(t_i + dt / 2, sol[step_i - 1] + dt * k2 / 2, u[step_i - 1])
-            k4 = sys.rhs(t_i + dt, sol[step_i - 1] + dt * k3, u[step_i - 1])
-        else:
-            k1 = sys.rhs(t_i, sol[step_i - 1])
-            k2 = sys.rhs(t_i + dt / 2, sol[step_i - 1] + dt * k1 / 2)
-            k3 = sys.rhs(t_i + dt / 2, sol[step_i - 1] + dt * k2 / 2)
-            k4 = sys.rhs(t_i + dt, sol[step_i - 1] + dt * k3)
 
-        sol[step_i] = sol[step_i - 1] + dt / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
+    if t is None:
+        t = 0
+
+    if u is not None:
+        k1 = sys.rhs(t, x, u)
+        k2 = sys.rhs(t + dt / 2, x + dt * k1 / 2, u)
+        k3 = sys.rhs(t + dt / 2, x + dt * k2 / 2, u)
+        k4 = sys.rhs(t + dt, x + dt * k3, u)
+    else:
+        k1 = sys.rhs(t, x)
+        k2 = sys.rhs(t + dt / 2, x + dt * k1 / 2)
+        k3 = sys.rhs(t + dt / 2, x + dt * k2 / 2)
+        k4 = sys.rhs(t + dt, x + dt * k3)
+
+    sol = x + dt / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
     time_end = time.perf_counter()
 
     meta_data = {
         'comp_time': time_end - time_start,
-        'n_steps': n_steps,
-        't_span': t_span,
         'dt': dt
     }
-    return t, sol, meta_data
+    return sol, meta_data
